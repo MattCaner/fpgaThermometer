70fd8cbe4bcc041362c496de450ad02132aaf69b
Jakub Salamon <33131677+Jacobsky98@users.noreply.github.com>
2020-11-23 09:28:00 +0100

    Fix top

--- PROJECT.srcs/sources_1/new/top.v
+++ PROJECT.srcs/sources_1/new/top.v
@@ -1,16 +1,15 @@
 `timescale 1ns / 1ps
 
 
-module top(input clk, rst, start);
+module top #(parameter nb = 9)(input clk, rst, start, wire3rst, wire3clk, wire3dq, output [nb-1:0] dataOut);
 
-    localparam hp = 5, brate = 9600, nb = 9;
+    localparam hp = 5, brate = 9600;
     reg slowclk;
-    reg ready, wire3dq, wire3clk, wire3rst;
     reg [nb-1:0] dataOut;
     
     reg trn, fin;
     
-    clkdiv clkdiv_mod (.clk(clk), .rst(rst), .slowclk(slowclk));
+//    clkdiv clkdiv_mod (.clk(clk), .rst(rst), .slowclk(slowclk));
     wire3 wire3_mod (.clk(slowclk), .rst(rst), .start(start), .wire3dq(wire3dq), .wire3clk(wire3clk), .wire3rst(wire3rst), .dataOut(dataOut), .ready(ready));
     // dodaæ przypisania do wire3dq, output wire3clk, wire3rst
     simple_transmitter #(.fclk(10**9/(2*hp)), .baudrate(brate), .nb(nb)) simple_transmitter_mod (.clk(clk), .rst(~rst), .str(ready), .val(dataOut), .trn(trn), .fin(fin));
